aa711af403a4d103ca05d4353557adf95c060a50
Daniel Melo <daniel.melo42@outlook.com>
2023-01-17 16:53:19 -0700

    color schema selected

---
+++ app/src/main/java/com/dicedev/thebigchampion/utils/AppColors.kt
@@ -0,0 +1,11 @@
+package com.dicedev.thebigchampion.utils
+
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val lightGray = Color(0xFFBAC1B8)
+    val darkGray = Color(0xFF2B303A)
+    val lightBlue = Color(0xFF58A4B0)
+    val solidGreen = Color(0xFF0C7C59)
+    val darkOrange = Color(0xFFD64933)
+}

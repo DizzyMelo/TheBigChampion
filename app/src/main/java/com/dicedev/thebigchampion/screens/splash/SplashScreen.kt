b205941e08add9a4a8c9fb4698af0ec2aafbb46e
Daniel Melo <daniel.melo42@outlook.com>
2023-01-17 16:53:53 -0700

    app icon changed

---
+++ app/src/main/java/com/dicedev/thebigchampion/screens/splash/SplashScreen.kt
@@ -0,0 +1,14 @@
+package com.dicedev.thebigchampion.screens.splash
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    Surface(modifier = Modifier.fillMaxSize()) {
+
+    }
+}

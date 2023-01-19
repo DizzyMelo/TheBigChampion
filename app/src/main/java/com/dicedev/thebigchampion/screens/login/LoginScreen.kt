5287dbb3d7878e23dbfdc58904d16bddaca50a94
Daniel Melo <daniel.melo42@outlook.com>
2023-01-18 17:02:56 -0700

    initial login screen implementation

---
+++ app/src/main/java/com/dicedev/thebigchampion/screens/login/LoginScreen.kt
@@ -0,0 +1,42 @@
+package com.dicedev.thebigchampion.screens.login
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.dicedev.thebigchampion.components.AppLogo
+import com.dicedev.thebigchampion.components.EmailTextField
+import com.dicedev.thebigchampion.components.PasswordTextField
+
+@Composable
+fun LoginScreen(navController: NavController) {
+    val emailState = remember {
+        mutableStateOf("")
+    }
+
+    val passwordState = remember {
+        mutableStateOf("")
+    }
+
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            AppLogo()
+            Spacer(modifier = Modifier.height(20.dp))
+            EmailTextField(emailState = emailState)
+            PasswordTextField(passwordState = passwordState)
+
+        }
+    }
+}

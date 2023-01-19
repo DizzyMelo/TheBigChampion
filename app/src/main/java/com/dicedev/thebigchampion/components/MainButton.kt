a8408c21a62ff7ccf715b89bf907bb6fd4fc0ed5
Daniel Melo <daniel.melo42@outlook.com>
2023-01-18 17:45:39 -0700

    added enabled and loading functionalities

--- app/src/main/java/com/dicedev/thebigchampion/components/MainButton.kt
+++ app/src/main/java/com/dicedev/thebigchampion/components/MainButton.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,11 +14,23 @@ import androidx.compose.ui.unit.dp
 import com.dicedev.thebigchampion.utils.AppColors
 
 @Composable
-fun MainButton(modifier: Modifier = Modifier, label: String, onClick: () -> Unit = {}) {
+fun MainButton(
+    modifier: Modifier = Modifier,
+    label: String,
+    isLoading: Boolean = false,
+    enabled: Boolean = true,
+    onClick: () -> Unit = {}
+) {
+    if (isLoading) {
+        return CircularProgressIndicator()
+    }
     Button(
         onClick = { onClick.invoke() },
-        modifier = modifier.fillMaxWidth().height(48.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .height(48.dp),
         shape = RoundedCornerShape(15.dp),
+        enabled = enabled,
         colors = ButtonDefaults.buttonColors(
             backgroundColor = AppColors.solidGreen,
             contentColor = Color.White

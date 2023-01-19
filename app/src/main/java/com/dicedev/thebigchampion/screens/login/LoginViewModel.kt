ef1bb7218b728e7b3a404ca1bf560495a1b47d43
Daniel Melo <daniel.melo42@outlook.com>
2023-01-18 18:07:37 -0700

    viewModelScope used to launch new coroutine

--- app/src/main/java/com/dicedev/thebigchampion/screens/login/LoginViewModel.kt
+++ app/src/main/java/com/dicedev/thebigchampion/screens/login/LoginViewModel.kt
@@ -4,25 +4,31 @@ import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
     val signInLoading: MutableState<Boolean> = mutableStateOf(false)
-    fun signInWithEmailAndPassword(email: String, password: String, callback: () -> Unit = {}) {
-        try {
-            signInLoading.value = true
-            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        callback.invoke()
-                    } else {
-                        Log.d("FB AUTh", "signInWithEmailAndPassword: email or password incorrect")
+    fun signInWithEmailAndPassword(email: String, password: String, callback: () -> Unit = {}) =
+        viewModelScope.launch {
+            try {
+                signInLoading.value = true
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            callback.invoke()
+                        } else {
+                            Log.d(
+                                "FB AUTh",
+                                "signInWithEmailAndPassword: email or password incorrect"
+                            )
+                        }
                     }
-                }
-        } catch (exception: Exception) {
-            Log.d("FB AUTh", "signInWithEmailAndPassword: ${exception.message}")
-        } finally {
-            signInLoading.value = false
+            } catch (exception: Exception) {
+                Log.d("FB AUTh", "signInWithEmailAndPassword: ${exception.message}")
+            } finally {
+                signInLoading.value = false
+            }
         }
-    }
 }

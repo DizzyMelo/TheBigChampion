379ef4f42986ce253683c09415adac036e5bdd72
Daniel Melo <daniel.melo42@outlook.com>
2023-01-19 21:37:30 -0700

    user view model with signup user method created

---
+++ app/src/main/java/com/dicedev/thebigchampion/screens/signup/SignupViewModel.kt
@@ -0,0 +1,40 @@
+package com.dicedev.thebigchampion.screens.signup
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dicedev.thebigchampion.data.FirebaseDao
+import com.dicedev.thebigchampion.models.User
+import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SignupViewModel @Inject constructor(
+    private val firebaseAuth: FirebaseAuth,
+    private val firebaseDao: FirebaseDao
+) : ViewModel() {
+    fun signupWithEmailAndPassword(email: String, password: String, onSuccessCallback: () -> Unit) =
+        viewModelScope.launch {
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val user = User(
+                            id = task.result.user?.uid,
+                            name = "test",
+                            email = email,
+                            photo = "no-photo"
+                        ).toMap()
+                        firebaseDao.addDocument(user).run {
+                            onSuccessCallback.invoke()
+                        }
+                    } else {
+                        Log.d(
+                            "SIGN UP",
+                            "signupWithEmailAndPassword: something went wrong trying to create user"
+                        )
+                    }
+                }
+        }
+}

8c9a1d244176633220dd7e69426c411aebf15254
Daniel Melo <daniel.melo42@outlook.com>
2023-01-19 18:27:39 -0700

    provide dependencies

--- app/src/main/java/com/dicedev/thebigchampion/di/AppModule.kt
+++ app/src/main/java/com/dicedev/thebigchampion/di/AppModule.kt
@@ -1,10 +1,22 @@
 package com.dicedev.thebigchampion.di
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideFirebaseAuth() = FirebaseAuth.getInstance()
+
+    @Singleton
+    @Provides
+    fun provideFirebaseFirestore() = FirebaseFirestore.getInstance()
 }

39d8c4c01d59b7764e02d242b203f8bc51adbb6d
Daniel Melo <daniel.melo42@outlook.com>
2023-01-19 21:37:10 -0700

    create user method implemented

---
+++ app/src/main/java/com/dicedev/thebigchampion/data/FirebaseDao.kt
@@ -0,0 +1,10 @@
+package com.dicedev.thebigchampion.data
+
+import com.google.firebase.firestore.FirebaseFirestore
+import javax.inject.Inject
+
+class FirebaseDao @Inject constructor(private val firestore: FirebaseFirestore) {
+    fun addDocument(document: HashMap<String, Any>) {
+        firestore.collection("users").add(document)
+    }
+}

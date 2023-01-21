51007639e7ab4cdb03199ea591772b9b86da6043
Daniel Melo <daniel.melo42@outlook.com>
2023-01-20 23:02:59 -0700

    section title component created

--- app/src/main/java/com/dicedev/thebigchampion/components/Components.kt
+++ app/src/main/java/com/dicedev/thebigchampion/components/Components.kt
@@ -3,9 +3,12 @@ package com.dicedev.thebigchampion.components
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
@@ -14,8 +17,10 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.dicedev.thebigchampion.R
 import com.dicedev.thebigchampion.utils.AppColors
 
@@ -45,4 +50,16 @@ fun FABIcon(onClick: () -> Unit) {
     ) {
         Icon(imageVector = Icons.Default.Add, contentDescription = "Add icon")
     }
+}
+
+@Composable
+fun SectionTitle(text: String, modifier: Modifier = Modifier, color: Color = AppColors.darkOrange) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.h4,
+        fontWeight = FontWeight.Bold,
+        fontSize = 17.sp,
+        color = color,
+        modifier = modifier.padding(vertical = 10.dp)
+    )
 }

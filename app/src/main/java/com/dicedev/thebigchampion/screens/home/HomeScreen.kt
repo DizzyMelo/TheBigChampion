dee650568181c8ed13b0b087c582eb6e44a197da
Daniel Melo <daniel.melo42@outlook.com>
2023-01-20 23:03:22 -0700

    group content impolemented

--- app/src/main/java/com/dicedev/thebigchampion/screens/home/HomeScreen.kt
+++ app/src/main/java/com/dicedev/thebigchampion/screens/home/HomeScreen.kt
@@ -4,15 +4,16 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.dicedev.thebigchampion.components.FABIcon
-import com.dicedev.thebigchampion.components.MainTopAppBar
-import com.dicedev.thebigchampion.components.UserStats
+import com.dicedev.thebigchampion.components.*
+import com.dicedev.thebigchampion.data.stubs.GroupStubs
+import com.dicedev.thebigchampion.models.Group
 
 @Preview
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -42,6 +43,24 @@ fun HomeContent() {
     ) {
         Column {
             UserStats()
+            GroupsContent()
+        }
+    }
+}
+
+@Composable
+fun GroupsContent(groups: List<Group> = GroupStubs.getPopulatedGroups()) {
+    Surface {
+        Column {
+            SectionTitle(text = "Groups")
+            Column {
+                if (groups.isEmpty()) {
+                    Text(text = "No group")
+                }
+                groups.forEach { group ->
+                    GroupRow(group)
+                }
+            }
         }
     }
 }

2dd4963d5b8b82334ebb6a1bf798ab4e573a14fb
Daniel Melo <daniel.melo42@outlook.com>
2023-01-20 23:25:13 -0700

    list all documents from a collection

--- app/src/main/java/com/dicedev/thebigchampion/data/FirebaseDao.kt
+++ app/src/main/java/com/dicedev/thebigchampion/data/FirebaseDao.kt
@@ -7,4 +7,8 @@ class FirebaseDao @Inject constructor(private val firestore: FirebaseFirestore) 
     fun addDocument(document: HashMap<String, Any>) {
         firestore.collection("users").add(document)
     }
+
+    fun getCollection(collectionName: String) {
+        firestore.collection(collectionName).get()
+    }
 }

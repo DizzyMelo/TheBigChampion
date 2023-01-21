4daf3f12e37dfc747b0879522be8c05a3d073e0f
Daniel Melo <daniel.melo42@outlook.com>
2023-01-20 23:02:13 -0700

    GroupRow component created

---
+++ app/src/main/java/com/dicedev/thebigchampion/components/GroupRow.kt
@@ -0,0 +1,43 @@
+package com.dicedev.thebigchampion.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.dicedev.thebigchampion.models.Group
+import com.dicedev.thebigchampion.utils.AppColors
+
+@Composable
+fun GroupRow(group: Group) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 5.dp),
+        shape = RoundedCornerShape(15.dp),
+        color = AppColors.lightGray.copy(alpha = .2f)
+    ) {
+        Row(
+            modifier = Modifier.padding(10.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Surface(
+                modifier = Modifier
+                    .size(50.dp)
+                    .padding(5.dp),
+                shape = CircleShape,
+                color = AppColors.solidGreen
+            ) {
+
+            }
+            Column(modifier = Modifier.padding(start = 10.dp)) {
+                Text(text = group.name)
+                Text(text = "4 players")
+            }
+        }
+    }
+}

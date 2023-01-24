644611f24bf7956ea14b01be6cda007e73edfa2b
Daniel Melo <daniel.melo42@outlook.com>
2023-01-23 23:22:35 -0700

    state flow extension added

--- app/src/main/java/com/dicedev/thebigchampion/data/FirebaseDao.kt
+++ app/src/main/java/com/dicedev/thebigchampion/data/FirebaseDao.kt
@@ -3,14 +3,36 @@ package com.dicedev.thebigchampion.data
 import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QuerySnapshot
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import javax.inject.Inject
 
 class FirebaseDao @Inject constructor(private val firestore: FirebaseFirestore) {
-    fun addDocument(collectionName: String, document: HashMap<String, Any>): Task<DocumentReference> {
+    fun addDocument(
+        collectionName: String,
+        document: HashMap<String, Any>
+    ): Task<DocumentReference> {
         return firestore.collection(collectionName).add(document)
     }
 
-    fun getCollection(collectionName: String) {
-        firestore.collection(collectionName).get()
+    fun getDocumentsFromCollection(collectionName: String): Flow<QuerySnapshot> {
+        return firestore.collection(collectionName).snapshotFlow()
+    }
+
+    private fun Query.snapshotFlow(): Flow<QuerySnapshot> = callbackFlow {
+        val listenerRegistration = addSnapshotListener { value, error ->
+            if (error != null) {
+                close()
+                return@addSnapshotListener
+            }
+            if (value != null)
+                trySend(value)
+        }
+        awaitClose {
+            listenerRegistration.remove()
+        }
     }
 }

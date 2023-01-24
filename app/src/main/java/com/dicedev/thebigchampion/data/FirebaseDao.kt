4838f657eddc16de95332d91e37f63c2c6d11926
Daniel Melo <daniel.melo42@outlook.com>
2023-01-23 20:44:50 -0700

    return task result

--- app/src/main/java/com/dicedev/thebigchampion/data/FirebaseDao.kt
+++ app/src/main/java/com/dicedev/thebigchampion/data/FirebaseDao.kt
@@ -1,11 +1,13 @@
 package com.dicedev.thebigchampion.data
 
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import javax.inject.Inject
 
 class FirebaseDao @Inject constructor(private val firestore: FirebaseFirestore) {
-    fun addDocument(document: HashMap<String, Any>) {
-        firestore.collection("users").add(document)
+    fun addDocument(collectionName: String, document: HashMap<String, Any>): Task<DocumentReference> {
+        return firestore.collection(collectionName).add(document)
     }
 
     fun getCollection(collectionName: String) {

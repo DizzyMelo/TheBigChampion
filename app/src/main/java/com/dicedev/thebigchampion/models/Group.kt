9a7c5a60766877a627d8366fff4069445eb9cd21
Daniel Melo <daniel.melo42@outlook.com>
2023-01-23 20:45:09 -0700

    toMap method added and default values

--- app/src/main/java/com/dicedev/thebigchampion/models/Group.kt
+++ app/src/main/java/com/dicedev/thebigchampion/models/Group.kt
@@ -1,3 +1,17 @@
 package com.dicedev.thebigchampion.models
 
-data class Group(val id: String, val name: String, val image: String, val members: List<String>)
+data class Group(
+    val id: String? = null,
+    val name: String,
+    val image: String? = null,
+    val members: List<String> = emptyList()
+) {
+    fun toMap(): HashMap<String, Any> {
+        return hashMapOf(
+            "id" to this.id.toString(),
+            "name" to this.name,
+            "image" to this.image.toString(),
+            "members" to members
+        )
+    }
+}

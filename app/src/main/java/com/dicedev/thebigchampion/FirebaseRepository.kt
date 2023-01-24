7f2a4a52bb934f3b7aa804889ceee7835113d549
Daniel Melo <daniel.melo42@outlook.com>
2023-01-23 23:22:46 -0700

    file renamed

---
+++ app/src/main/java/com/dicedev/thebigchampion/FirebaseRepository.kt
@@ -0,0 +1,21 @@
+package com.dicedev.thebigchampion
+
+import com.dicedev.thebigchampion.data.FirebaseDao
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.QuerySnapshot
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class FirebaseRepository @Inject constructor(private val dao: FirebaseDao) {
+    fun insertDocument(
+        collectionName: String,
+        document: HashMap<String, Any>
+    ): Task<DocumentReference> {
+        return dao.addDocument(collectionName, document)
+    }
+
+    fun getDocuments(collectionName: String): Flow<QuerySnapshot> {
+        return dao.getDocumentsFromCollection(collectionName)
+    }
+}

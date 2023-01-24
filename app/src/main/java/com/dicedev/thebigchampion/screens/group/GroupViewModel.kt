d295584aafe59a28df34935f2aa4b18e270d9c67
Daniel Melo <daniel.melo42@outlook.com>
2023-01-23 23:24:12 -0700

    listeners changed

--- app/src/main/java/com/dicedev/thebigchampion/screens/group/GroupViewModel.kt
+++ app/src/main/java/com/dicedev/thebigchampion/screens/group/GroupViewModel.kt
@@ -3,7 +3,7 @@ package com.dicedev.thebigchampion.screens.group
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.dicedev.thebigchampion.data.FirebaseDao
+import com.dicedev.thebigchampion.FirebaseRepository
 import com.dicedev.thebigchampion.models.Group
 import com.dicedev.thebigchampion.utils.CollectionNames
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,18 +11,16 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class GroupViewModel @Inject constructor(private val firebaseDao: FirebaseDao) : ViewModel() {
+class GroupViewModel @Inject constructor(private val repository: FirebaseRepository) : ViewModel() {
     fun createGroup(name: String, onSuccessAction: () -> Unit = {}) = viewModelScope.launch {
         try {
-            firebaseDao.addDocument(
+            repository.insertDocument(
                 collectionName = CollectionNames.GROUPS,
                 document = Group(name = name).toMap()
-            ).addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    onSuccessAction.invoke()
-                } else {
-                    Log.d("FB Firestore", "createGroup: the task failed")
-                }
+            ).addOnSuccessListener {
+                onSuccessAction.invoke()
+            }.addOnFailureListener {
+                Log.d("FB Firestore", "createGroup: the task failed")
             }
         } catch (exception: Exception) {
             Log.d("FB Firestore", "createGroup: Exception ${exception.message}")

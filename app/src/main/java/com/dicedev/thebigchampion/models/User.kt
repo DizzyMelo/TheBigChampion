19ddbaf54dbf18252d8fad970cf1b1e0bc45c88d
Daniel Melo <daniel.melo42@outlook.com>
2023-01-27 15:45:01 -0700

    userId attribute added

--- app/src/main/java/com/dicedev/thebigchampion/models/User.kt
+++ app/src/main/java/com/dicedev/thebigchampion/models/User.kt
@@ -2,6 +2,7 @@ package com.dicedev.thebigchampion.models
 
 data class User(
     var id: String? = null,
+    var userId: String? = null,
     var name: String? = null,
     var email: String? = null,
     var photo: String? = null
@@ -9,6 +10,7 @@ data class User(
     fun toMap(): HashMap<String, Any> {
         return hashMapOf(
             "id" to this.id.toString(),
+            "userId" to this.userId.toString(),
             "name" to this.name.toString(),
             "email" to this.email.toString(),
             "photo" to this.photo.toString(),

d0c00ea94256f60948ac8d378c58915245508dfb
Daniel Melo <daniel.melo42@outlook.com>
2023-01-27 16:36:26 -0700

    login process improved

--- app/src/main/java/com/dicedev/thebigchampion/screens/login/LoginScreen.kt
+++ app/src/main/java/com/dicedev/thebigchampion/screens/login/LoginScreen.kt
@@ -1,25 +1,32 @@
 package com.dicedev.thebigchampion.screens.login
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.dicedev.thebigchampion.components.AppLogo
 import com.dicedev.thebigchampion.components.EmailTextField
 import com.dicedev.thebigchampion.components.MainButton
 import com.dicedev.thebigchampion.components.PasswordTextField
 import com.dicedev.thebigchampion.navigation.AppScreens
+import kotlinx.coroutines.launch
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun LoginScreen(navController: NavController, loginViewModel: LoginViewModel = LoginViewModel()) {
+fun LoginScreen(navController: NavController, loginViewModel: LoginViewModel = hiltViewModel()) {
     val emailState = rememberSaveable {
         mutableStateOf("")
     }
@@ -32,38 +39,51 @@ fun LoginScreen(navController: NavController, loginViewModel: LoginViewModel = L
         emailState.value.trim().isNotEmpty() && passwordState.value.trim().isNotEmpty()
     }
 
-    Surface(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(20.dp)
-    ) {
-        Column(
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(scaffoldState = scaffoldState) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp),
         ) {
-            AppLogo()
-            Spacer(modifier = Modifier.height(20.dp))
-            EmailTextField(emailState = emailState)
-            PasswordTextField(passwordState = passwordState)
-            MainButton(
-                label = "Login",
-                isLoading = loginViewModel.signInLoading.value,
-                enabled = valid
+            Column(
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                loginViewModel.signInWithEmailAndPassword(
-                    emailState.value,
-                    passwordState.value,
-                    callback = {
-                        navController.navigate(AppScreens.HomeScreen.name) {
-                            popUpTo(AppScreens.HomeScreen.name) { inclusive = true }
+                AppLogo()
+                Spacer(modifier = Modifier.height(20.dp))
+                EmailTextField(emailState = emailState)
+                PasswordTextField(passwordState = passwordState)
+                MainButton(
+                    label = "Login",
+                    isLoading = loginViewModel.signInLoading.value,
+                    enabled = valid
+                ) {
+                    loginViewModel.signInWithEmailAndPassword(
+                        emailState.value,
+                        passwordState.value,
+                        onSuccessCallback = {
+                            navController.navigate(AppScreens.HomeScreen.name) {
+                                popUpTo(AppScreens.HomeScreen.name) { inclusive = true }
+                            }
+                        },
+                        onFailureCallback = {
+                            coroutineScope.launch {
+                                scaffoldState.snackbarHostState.showSnackbar(
+                                    message = "Email or password are not correct",
+                                    actionLabel = "Ok"
+                                )
+                            }
                         }
-                    }
-                )
+                    )
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(text = "Create an account", modifier = Modifier.clickable {
+                    navController.navigate(AppScreens.SignupScreen.name)
+                })
             }
-            Spacer(modifier = Modifier.height(20.dp))
-            Text(text = "Create an account", modifier = Modifier.clickable {
-                navController.navigate(AppScreens.SignupScreen.name)
-            })
         }
     }
 }

4ca3561e1286fb7dd52ce4c9b84e1747d3124656
Daniel Melo <daniel.melo42@outlook.com>
2023-01-27 18:27:33 -0700

    snackbar message added

--- app/src/main/java/com/dicedev/thebigchampion/screens/group/CreateGroupScreen.kt
+++ app/src/main/java/com/dicedev/thebigchampion/screens/group/CreateGroupScreen.kt
@@ -6,9 +6,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -18,6 +20,7 @@ import com.dicedev.thebigchampion.components.MainButton
 import com.dicedev.thebigchampion.components.MainTextField
 import com.dicedev.thebigchampion.components.MainTopAppBar
 import com.dicedev.thebigchampion.navigation.AppScreens
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -25,6 +28,9 @@ fun CreateGroupScreen(
     navController: NavController,
     groupViewModel: GroupViewModel = hiltViewModel()
 ) {
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+
     Scaffold(
         topBar = {
             MainTopAppBar(
@@ -35,7 +41,8 @@ fun CreateGroupScreen(
             FABIcon(icon = Icons.Default.ArrowForward) {
 
             }
-        }) {
+        }, scaffoldState = scaffoldState
+    ) {
         val groupName = remember {
             mutableStateOf("")
         }
@@ -46,9 +53,15 @@ fun CreateGroupScreen(
         Column(modifier = Modifier.padding(10.dp)) {
             MainTextField(valueState = groupName, label = "Group Name")
             MainButton(label = "Next", enabled = valid) {
-                groupViewModel.createGroup(groupName.value) {
-                    navController.navigate(AppScreens.AddPlayersToGroupScreen.name)
-                }
+                groupViewModel.createGroup(
+                    groupName.value,
+                    onSuccessAction = { navController.navigate(AppScreens.AddPlayersToGroupScreen.name) },
+                    onFailureAction = {
+                        coroutineScope.launch {
+                            scaffoldState.snackbarHostState.showSnackbar("Something went wrong trying to create the group")
+                        }
+                    }
+                )
             }
         }
     }

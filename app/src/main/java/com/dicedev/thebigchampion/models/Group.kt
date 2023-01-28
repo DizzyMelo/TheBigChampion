58dcbf1121f2e8ac03c1dc103bcacbd7303bd504
Daniel Melo <daniel.melo42@outlook.com>
2023-01-27 17:02:13 -0700

    ownerId field added

--- app/src/main/java/com/dicedev/thebigchampion/models/Group.kt
+++ app/src/main/java/com/dicedev/thebigchampion/models/Group.kt
@@ -3,6 +3,7 @@ package com.dicedev.thebigchampion.models
 data class Group(
     val id: String? = null,
     val name: String,
+    val ownerId: String,
     val image: String? = null,
     val players: List<String> = emptyList(),
     val games: List<String> = emptyList(),
@@ -12,6 +13,7 @@ data class Group(
         return hashMapOf(
             "id" to this.id.toString(),
             "name" to this.name,
+            "ownerId" to this.ownerId,
             "image" to this.image.toString(),
             "players" to players,
             "games" to games,

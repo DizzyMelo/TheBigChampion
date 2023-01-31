c6fead95f94814509627afe9a4344827cb4899e6
Daniel Melo <daniel.melo42@outlook.com>
2023-01-30 17:04:17 -0700

    display loading component while firebase sign process is is progress

--- app/src/main/java/com/dicedev/thebigchampion/screens/signup/SignupScreen.kt
+++ app/src/main/java/com/dicedev/thebigchampion/screens/signup/SignupScreen.kt
@@ -1,12 +1,16 @@
 package com.dicedev.thebigchampion.screens.signup
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,7 +22,9 @@ import com.dicedev.thebigchampion.components.EmailTextField
 import com.dicedev.thebigchampion.components.MainButton
 import com.dicedev.thebigchampion.components.PasswordTextField
 import com.dicedev.thebigchampion.navigation.AppScreens
+import kotlinx.coroutines.launch
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun SignupScreen(navController: NavController, signupViewModel: SignupViewModel = hiltViewModel()) {
     val emailState = rememberSaveable {
@@ -33,38 +39,48 @@ fun SignupScreen(navController: NavController, signupViewModel: SignupViewModel 
         emailState.value.trim().isNotEmpty() && passwordState.value.trim().isNotEmpty()
     }
 
-    Surface(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(20.dp)
-    ) {
-        Column(
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(scaffoldState = scaffoldState) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp)
         ) {
-            AppLogo()
-            Spacer(modifier = Modifier.height(20.dp))
-            EmailTextField(emailState = emailState)
-            PasswordTextField(passwordState = passwordState)
-            MainButton(
-                label = "Sign up",
-                isLoading = false,
-                enabled = valid
+            Column(
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                signupViewModel.signupWithEmailAndPassword(
-                    emailState.value,
-                    passwordState.value,
-                    onSuccessCallback = {
-                        navController.navigate(AppScreens.HomeScreen.name) {
-                            popUpTo(AppScreens.HomeScreen.name) { inclusive = true }
+                AppLogo()
+                Spacer(modifier = Modifier.height(20.dp))
+                EmailTextField(emailState = emailState)
+                PasswordTextField(passwordState = passwordState)
+                MainButton(
+                    label = "Sign up",
+                    isLoading = signupViewModel.isLoading.value,
+                    enabled = valid
+                ) {
+                    signupViewModel.signupWithEmailAndPassword(
+                        emailState.value,
+                        passwordState.value,
+                        onSuccessCallback = {
+                            navController.navigate(AppScreens.HomeScreen.name) {
+                                popUpTo(AppScreens.HomeScreen.name) { inclusive = true }
+                            }
+                        },
+                        onFailureCallback = {
+                            coroutineScope.launch {
+                                scaffoldState.snackbarHostState.showSnackbar("Sorry, something went wrong trying to sign up the user.")
+                            }
                         }
-                    }
-                )
+                    )
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(text = "Login", modifier = Modifier.clickable {
+                    navController.navigate(AppScreens.LoginScreen.name)
+                })
             }
-            Spacer(modifier = Modifier.height(20.dp))
-            Text(text = "Login", modifier = Modifier.clickable {
-                navController.navigate(AppScreens.LoginScreen.name)
-            })
         }
     }
 }
